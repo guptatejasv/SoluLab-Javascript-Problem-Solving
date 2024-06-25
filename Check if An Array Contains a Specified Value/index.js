ca7007737ebf60eaa51bacd52c0c6e021397e72e
Tejasv Kumar <guptatejasv86086@gmail.com>
2024-06-25 11:57:59 +0530

    20 program added

---
+++ Check if An Array Contains a Specified Value/index.js
@@ -0,0 +1,8 @@
+let array = [1, 2, 3, 4, 5];
+let valueToCheck = 3;
+
+if (array.includes(valueToCheck)) {
+  console.log(`Array contains ${valueToCheck}`);
+} else {
+  console.log(`Array does not contain ${valueToCheck}`);
+}
