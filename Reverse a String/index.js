7737cd35a2a1893f9952c7626decfdd2896abb48
Tejasv Kumar <guptatejasv86086@gmail.com>
2024-06-26 10:32:09 +0530

    29 program added

---
+++ Reverse a String/index.js
@@ -0,0 +1,7 @@
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
+
+let originalString = "hello";
+let reversedString = reverseString(originalString);
+console.log(reversedString);
